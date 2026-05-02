437947e8141c55ecd7d36dcc2a8cf86ca2597cdc
Ahmed Hussien <159754671+Ahmed2222002@users.noreply.github.com>
2026-05-02 16:51:23 +0300

    feat: connect MongoDB on application startup
    Add the MongoDB connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import connectDB from './config/dbConnect.js';
 import { Environment } from './config/configEnvironment.js';
 
 const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 app.listen(Environment.PORT, () => {
   console.log(`Server is running in ${Environment.NODE_ENV} environment on port ${Environment.PORT}`);
