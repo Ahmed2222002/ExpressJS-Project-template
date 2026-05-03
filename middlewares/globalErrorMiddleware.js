bcf4f813a1e6436d7e4223a76080910c61cb6a5a
Ahmed Hussien <159754671+Ahmed2222002@users.noreply.github.com>
2026-05-03 22:55:58 +0300

    edit global error middleware

--- middlewares/globalErrorMiddleware.js
+++ middlewares/globalErrorMiddleware.js
@@ -1,3 +1,5 @@
+import { errorResponse } from "../utils/responses";
+
 function globalErrorMiddleware(err, req, res, next) {
     if (process.env.NODE_ENV === 'development') {
         sendErrorForDevelopment(err, res);
@@ -6,19 +8,26 @@ function globalErrorMiddleware(err, req, res, next) {
     }
 }
 
+// response with full error stack in development, but only message in production
+
 function sendErrorForDevelopment(err, res) {
-    res.status(500).json({
-        status: 'error',
-        message: err.message,
-        stack: err.stack
-    });
+    errorResponse(
+        res,
+        err.statusCode || 500,
+        err.message || 'Internal Server Error',
+        {
+            stack: err.stack
+        }
+    );
 }
 
 function sendErrorForProduction(err, res) {
-    res.status(500).json({
-        status: 'error',
-        message: 'Internal Server Error'
-    });
+    errorResponse(
+        res, 
+        err.statusCode || 500, 
+        'Internal Server Error'
+    );
 }
+
 
 export default globalErrorMiddleware;
